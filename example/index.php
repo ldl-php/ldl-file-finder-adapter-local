fec835a608fc61302f26eea8f3c3f7e29b00581e
Federico <jpfstange@gmail.com>
2021-04-15 16:58:57 -0300

    First commit

---
+++ example/index.php
@@ -0,0 +1,75 @@
+<?php declare(strict_types=1);
+
+require __DIR__.'/../vendor/autoload.php';
+
+use LDL\File\Finder\Adapter\Local\Facade\LocalFileFinderFacade;
+use LDL\File\Finder\FoundFile;
+use LDL\Validators\Chain\ValidatorChainInterface;
+use LDL\File\Validator\FileTypeValidator;
+use LDL\File\Validator\Config\FileTypeValidatorConfig;
+use LDL\File\Validator\FileSizeValidator;
+use LDL\File\Validator\Config\FileSizeValidatorConfig;
+use LDL\File\Validator\HasRegexContentValidator;
+use LDL\Validators\HasValidatorResultInterface;
+use LDL\Validators\Chain\OrValidatorChain;
+use LDL\Validators\Chain\AndValidatorChain;
+use LDL\Validators\RegexValidator;
+
+try{
+
+    echo "[ Find ]\n";
+
+    if(!isset($_SERVER['argv'][1], $_SERVER['argv'][2])){
+        die(sprintf('Usage: %s <dir1, dir2> <regex>%s', basename(__FILE__), "\n\n"));
+    }
+
+    $files = explode(',', $_SERVER['argv'][1]);
+    $match = $_SERVER['argv'][2];
+
+    $start = hrtime(true);
+
+    $r = LocalFileFinderFacade::findResult(
+        $files,
+        new OrValidatorChain([
+            new AndValidatorChain([
+                new FileTypeValidator([FileTypeValidatorConfig::FILE_TYPE_REGULAR]),
+                new FileSizeValidator(1000000, FileSizeValidatorConfig::OPERATOR_LTE),
+                new HasRegexContentValidator($match, true)
+            ]),
+            new AndValidatorChain([
+                new FileTypeValidator([FileTypeValidatorConfig::FILE_TYPE_DIRECTORY]),
+                new RegexValidator($match)
+            ])
+        ]),
+        [
+          static function (string $path){
+            dump('Accept:', $path);
+          }
+        ]
+    );
+
+    dump("DUMP FOUND FILES");
+    dump($r->count());
+    /**
+     * @var FoundFile $f
+     */
+    foreach($r as $f){
+        echo "File: $f\n";
+        /**
+         * @var HasValidatorResultInterface $v
+         */
+        //foreach($f->getValidatorChain() as $v){
+         //   var_dump($v->getResult());
+        //}
+    }
+
+    $end = hrtime(true);
+
+    echo sprintf('Search took: %s milliseconds %s', ($end-$start)/1e+6,"\n\n");
+}catch(\Exception $e) {
+
+    echo "[ Finder failed! ]\n";
+    var_dump($e);
+
+}
+
---
+++ src/LDL/File/Finder/Adapter/Local/LocalFileFinderAdapter.php
@@ -0,0 +1,136 @@
+<?php declare(strict_types=1);
+
+namespace LDL\File\Finder\Adapter\Local;
+
+use LDL\Framework\Base\Collection\CallableCollection;
+use LDL\File\Finder\Adapter\AdapterInterface;
+use LDL\File\Finder\FoundFile;
+use LDL\Validators\Chain\ValidatorChainInterface;
+
+class LocalFileFinderAdapter implements AdapterInterface
+{
+    /**
+     * @var ValidatorChainInterface
+     */
+    private $validators;
+
+    /**
+     * @var CallableCollection
+     */
+    private $onFile;
+
+    /**
+     * @var CallableCollection
+     */
+    private $onReject;
+
+    /**
+     * @var CallableCollection
+     */
+    private $onAccept;
+
+    /**
+     * Holds already traversed files (as keys) to avoid symlink recursion
+     * @var array
+     */
+    private $files = [];
+
+    private $count;
+
+    public function __construct(ValidatorChainInterface $validatorChain = null)
+    {
+        $this->validators = $validatorChain;
+        $this->onFile = new CallableCollection();
+        $this->onReject = new CallableCollection();
+        $this->onAccept = new CallableCollection();
+    }
+
+    public function onFile() : CallableCollection
+    {
+        return $this->onFile;
+    }
+
+    public function onReject(): CallableCollection
+    {
+        return $this->onReject;
+    }
+
+    public function onAccept() : CallableCollection
+    {
+        return $this->onAccept;
+    }
+
+    public function find(iterable $directories): iterable
+    {
+        foreach($directories as $dir){
+            if(!is_string($dir)){
+                continue;
+            }
+
+            if(!is_dir($dir)){
+                continue;
+            }
+
+            $files = scandir($dir);
+
+            foreach($files as $file){
+                if('.' === $file || '..' === $file){
+                    continue;
+                }
+
+                $file = sprintf('%s%s%s', $dir, DIRECTORY_SEPARATOR, $file);
+
+                if(array_key_exists($file, $this->files)){
+                    continue;
+                }
+
+                $this->onFile()->call($file);
+
+                $this->count++;
+
+                $this->files[$file] = true;
+
+                if(!is_readable($file)){
+                    continue;
+                }
+
+                if(is_dir($file)){
+                    yield from $this->find([$file]);
+                    continue;
+                }
+
+                try{
+                    if(null !== $this->validators) {
+                        $this->validators->validate($file);
+                    }
+
+                    $foundFile = new FoundFile(
+                        $file,
+                        new \SplFileInfo($file),
+                        //$this->validators->filterByInterface(HasValidatorResultInterface::class)
+                    );
+
+                    $this->onAccept()->call($file, $this->validators);
+
+                    yield $foundFile;
+                }catch(\Exception $e){
+
+                    $this->onReject()->call($file, $this->validators);
+
+                    continue;
+                }
+            }
+        }
+    }
+
+    public function getTotalFileCount(): int
+    {
+        return $this->count;
+    }
+
+    public function getValidatorChain(): ValidatorChainInterface
+    {
+        return $this->validators;
+    }
+
+}
